c198fa4c80340d415684081656069f174d1e17e5
Hendi Santika <hendisantika@gmail.com>
2017-11-10 06:40:16 +0700

    #5 - Add Twitter Login Controller

---
+++ src/main/java/com/hendisantika/twitterlogin/controller/TwitterLoginController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.twitterlogin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TwitterLoginController {
+	
+    //starting page for Twitter login demo
+    @RequestMapping("/twitterLogin")
+    public String twiterLogin(Model model) {
+    	return "twitterLogin";
+    }
+    
+    
+    //redirect to demo if user hits the root
+    @RequestMapping("/")
+    public String home(Model model) {
+    	return "redirect:twitterLogin";
+    }
+}
